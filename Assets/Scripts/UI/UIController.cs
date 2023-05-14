6130c5a877ace8281c660b2103879a8a2f4bffee
AzDarche <alanmiranda57@hotmai.com>
2023-05-14 14:49:47 -0600

    Agregado Seleecion de nave

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using ShmupBoss;
 
 namespace UI {
     public class UIController : MonoBehaviour {
 
         public UnityEvent mainMenuEvent;
         private bool _mainMenuSet;
+        public UnityEvent playerMenuEvent;
+        private bool _playerMenuSet;
         public UnityEvent settingsMenuEvent;
+        [SerializeField]
+        private RectTransform[] avatarCanvases;
         private bool _settingsMenuSet;
         public UnityEvent exitWindowEvent;
+
+        private int avatarIndex = 0;
     
         public void ActivateMainMenu() {
             if (_mainMenuSet)
@@ -16,6 +24,14 @@ namespace UI {
             mainMenuEvent.Invoke();
             _mainMenuSet = true;
             _settingsMenuSet = false;
+            _playerMenuSet = false;
+        }
+
+        public void SelectedPlayer()
+        {
+            GameManager.Instance.AssignSelectedPlayer(avatarIndex);
+            Debug.Log("se ha seleccionado la nave" + avatarIndex);
+            SceneManager.LoadScene("SampleScene");
         }
 
         public void MainSetBool() {
@@ -27,6 +43,15 @@ namespace UI {
                 return;
             settingsMenuEvent.Invoke();
             _settingsMenuSet = true;
+        }
+
+        public void ActivatePlayerMenu()
+        {
+            if (_playerMenuSet)
+                return;
+            playerMenuEvent.Invoke();
+            _playerMenuSet = true;
+            EnableAvatarCanvas(avatarIndex);
         }
 
         public void ExitGame() {
@@ -46,6 +71,45 @@ namespace UI {
 
         public void ActivateExitWindow() {
             exitWindowEvent.Invoke();
+        }
+        
+        private void EnableAvatarCanvas(int index)
+        {
+            avatarCanvases[index].gameObject.SetActive(true);
+        }
+        private void DisableAllAvatarCanvases()
+        {
+            foreach (RectTransform avatarCanvas in avatarCanvases)
+            {
+                avatarCanvas.gameObject.SetActive(false);
+            }
+        }
+        public void NextAvatar()
+        {
+            if (avatarIndex + 1 > avatarCanvases.Length - 1)
+            {
+                return;
+            }
+
+            avatarIndex++;
+
+            DisableAllAvatarCanvases();
+            EnableAvatarCanvas(avatarIndex);
+            
+        }
+
+        public void PreviousAvatar()
+        {
+            if (avatarIndex - 1 < 0)
+            {
+                return;
+            }
+
+            avatarIndex--;
+
+            DisableAllAvatarCanvases();
+            EnableAvatarCanvas(avatarIndex);
+            
         }
 
         public void PlayEvent() {
